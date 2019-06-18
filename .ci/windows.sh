d63b9487f822a51938edcb4b712d664d05d900bf
Akman <akman.ru@gmail.com>
2019-06-18 02:54:24 +0200

    Fixed windows CI settings

--- .ci/windows.sh
+++ .ci/windows.sh
@@ -3,3 +3,10 @@
 7z x -bd $JPACKAGE_ARCHIVE_NAME
 
 choco install adoptopenjdk --version 11.0.2.9
+
+export JAVA_HOME="/c/Program Files/AdoptOpenJDK/jdk-11.0.2+9"
+export PATH="/c/Program Files/AdoptOpenJDK/jdk-11.0.2+9/bin":$PATH
+
+. $PROFILE
+
+refreshenv
