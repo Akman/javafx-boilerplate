8a5d2c9f87f6a4888138cb52bce2220425c43b01
Akman <akman.ru@gmail.com>
2019-06-18 03:51:12 +0200

    Fixed windows CI settings

--- .ci/osx.sh
+++ .ci/osx.sh
@@ -4,3 +4,8 @@ tar xzfp $JPACKAGE_ARCHIVE_NAME
 
 # chmod +x .ci/osx-add-key.sh
 # sh .ci/osx-add-key.sh
+
+# if [ -n "${TRAVIS_TAG}" -a "${TRAVIS_OS_NAME}" = "osx" ]; then
+#   openssl aes-256-cbc -k "$ENC_SECRET" -in app/assets/osx/codesign.cer.enc -out app/assets/osx/codesign.cer -d;
+#   openssl aes-256-cbc -k "$ENC_SECRET" -in app/assets/osx/codesign.p12.enc -out app/assets/osx/codesign.p12 -d;
+# fi
--- .ci/windows.sh
+++ .ci/windows.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+choco install wixtoolset
+choco install innosetup
+
 7z x -bd $JPACKAGE_ARCHIVE_NAME
 
 choco install adoptopenjdk --version 11.0.2.9
 
 echo 'export JAVA_HOME="/c/Program Files/AdoptOpenJDK/jdk-11.0.2+9"' >> ~/.profile
 echo 'export PATH="/c/Program Files/AdoptOpenJDK/jdk-11.0.2+9/bin":$PATH' >> ~/.profile
+
