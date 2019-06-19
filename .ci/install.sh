5d0af04cdcbef5f4a67278f66b9d89cc073e2cbf
Akman <akman.ru@gmail.com>
2019-06-19 22:03:55 +0200

    Added Travis CI deploy settings

--- .ci/install.sh
+++ .ci/install.sh
@@ -6,6 +6,10 @@ set -ev
 
 echo 'export GRADLE_OPTS=-Dfile.encoding=utf-8' > .travis.env
 
+if [ -n "${TRAVIS_TAG}" ]; then
+  echo "export VERSION=${TRAVIS_TAG}" >> .travis.env
+fi
+
 if [ -f .ci/${TRAVIS_OS_NAME}/install.sh ]; then
   .ci/${TRAVIS_OS_NAME}/install.sh
 fi
