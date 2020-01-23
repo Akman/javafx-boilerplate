fb8c044b2855885b7cc078c40efb53dde78a55de
Akman <akman.ru@gmail.com>
2020-01-24 01:25:54 +0200

    Bump version 'v0.36.0'.

--- ci/linux/install.sh
+++ ci/linux/install.sh
@@ -12,8 +12,6 @@ wget -q $JPACKAGE_JDK_BASE_URL/$JPACKAGE_JDK_ARCHIVE_NAME
 tar xzfp $JPACKAGE_JDK_ARCHIVE_NAME
 echo "export BADASS_JLINK_JPACKAGE_HOME=$(pwd)/$JPACKAGE_JDK_HOME" >> .travis.env
 
-echo 'export GRADLE_OPTS="-Dorg.gradle.daemon=true"' >> .travis.env
-
 sudo apt-get update -y
 sudo apt-get install -y fakeroot
 sudo apt-get install -y rpm
--- ci/osx/install.sh
+++ ci/osx/install.sh
@@ -11,5 +11,3 @@ JPACKAGE_JDK_HOME=jdk-14.jdk/Contents/Home
 wget -q $JPACKAGE_JDK_BASE_URL/$JPACKAGE_JDK_ARCHIVE_NAME
 tar xzfp $JPACKAGE_JDK_ARCHIVE_NAME
 echo "export BADASS_JLINK_JPACKAGE_HOME=$(pwd)/$JPACKAGE_JDK_HOME" >> .travis.env
-
-echo 'export GRADLE_OPTS="-Dorg.gradle.daemon=true"' >> .travis.env
