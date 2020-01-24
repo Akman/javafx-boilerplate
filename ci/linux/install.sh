091e96231dc15e15b514be73f408858a60304a32
Akman <akman.ru@gmail.com>
2020-01-24 02:45:01 +0200

    Fixed CI install script for Windows

--- ci/linux/install.sh
+++ ci/linux/install.sh
@@ -7,7 +7,6 @@ set -ev
 JPACKAGE_JDK_BASE_URL=https://download.java.net/java/early_access/jdk14/32/GPL
 JPACKAGE_JDK_ARCHIVE_NAME=openjdk-14-ea+32_linux-x64_bin.tar.gz
 JPACKAGE_JDK_HOME=jdk-14
-
 wget -q $JPACKAGE_JDK_BASE_URL/$JPACKAGE_JDK_ARCHIVE_NAME
 tar xzfp $JPACKAGE_JDK_ARCHIVE_NAME
 echo "export BADASS_JLINK_JPACKAGE_HOME=$(pwd)/$JPACKAGE_JDK_HOME" >> .travis.env
--- ci/windows/install.sh
+++ ci/windows/install.sh
@@ -7,7 +7,6 @@ set -ev
 JPACKAGE_JDK_BASE_URL=https://download.java.net/java/early_access/jdk14/32/GPL
 JPACKAGE_JDK_ARCHIVE_NAME=openjdk-14-ea+32_windows-x64_bin.zip
 JPACKAGE_JDK_HOME=jdk-14
-
 wget -q $JPACKAGE_JDK_BASE_URL/$JPACKAGE_JDK_ARCHIVE_NAME
 7z x -bd $JPACKAGE_JDK_ARCHIVE_NAME
 echo "export BADASS_JLINK_JPACKAGE_HOME=$(pwd)/$JPACKAGE_JDK_HOME" >> .travis.env
@@ -20,13 +19,10 @@ echo "export BADASS_JLINK_JPACKAGE_HOME=$(pwd)/$JPACKAGE_JDK_HOME" >> .travis.en
 # FeatureJNLPFileRunWith	Associate .jnlp files with IcedTea-web
 # FeatureOracleJavaSoft	Updates registry keys HKLM/SOFTWARE/JavaSoft/
 choco install adoptopenjdk --version 13.102.8 -y -ia ADDLOCAL=FeatureMain,FeatureEnvironment,FeatureJavaHome
-
 echo 'export JAVA_HOME="/c/Program Files/AdoptOpenJDK/jdk-13.0.2.8-hotspot"' >> .travis.env
 
-choco install nuget.commandline
-nuget install WiX.Toolset
+wget -q https://github.com/wixtoolset/wix3/releases/download/wix3112rtm/wix311-binaries.zip
+7z x -bd -owixtoolset wix311-binaries.zip
+echo 'export WIX=$(pwd)/wixtoolset' >> .travis.env
 
-echo 'export PATH="/c/Program Files/AdoptOpenJDK/jdk-13.0.2.8-hotspot/bin":"/c/Program Files (x86)/WiX Toolset v3.11/bin":$PATH' >> .travis.env
-
-#echo 'export GRADLE_OPTS="-Dorg.gradle.daemon=false"' >> .travis.env
-
+echo 'export PATH="$JAVA_HOME/bin":"$WIX":$PATH' >> .travis.env
