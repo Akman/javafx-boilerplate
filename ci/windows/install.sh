9e918599594b37656a9ce90073a4c748104e825f
Alexander Kapitman <akman.ru@gmail.com>
2022-07-06 17:39:28 +0200

    Fixed CI settings

--- ci/windows/install.sh
+++ ci/windows/install.sh
@@ -7,27 +7,27 @@ set -ev
 JAVA_JDK_BASE_URL=https://download.bell-sw.com/java/18.0.1+12
 JAVA_JDK_ARCHIVE_NAME=bellsoft-jdk18.0.1+12-windows-amd64.zip
 JAVA_JDK_HOME=jdk-18.0.1
-
-wget -q $JAVA_JDK_BASE_URL/$JAVA_JDK_ARCHIVE_NAME
-7z x -bd $JAVA_JDK_ARCHIVE_NAME
 echo "export JAVA_HOME=$(pwd)/$JAVA_JDK_HOME" >> .travis.env
 
 JPACKAGE_JDK_BASE_URL=https://download.java.net/java/GA/jdk14.0.2/205943a0976c4ed48cb16f1043c5c647/12/GPL
 JPACKAGE_JDK_ARCHIVE_NAME=openjdk-14.0.2_windows-x64_bin.zip
 JPACKAGE_JDK_HOME=jdk-14.0.2
-
-wget -q $JPACKAGE_JDK_BASE_URL/$JPACKAGE_JDK_ARCHIVE_NAME
-7z x -bd $JPACKAGE_JDK_ARCHIVE_NAME
 echo "export JPACKAGE_HOME=$(pwd)/$JPACKAGE_JDK_HOME" >> .travis.env
 
 WIX_BASE_URL=https://github.com/wixtoolset/wix3/releases/download/wix3112rtm
 WIX_ARCHIVE_NAME=wix311-binaries.zip
 WIX_HOME=wix-3.11.2
+echo "export WIX=$(pwd)/$WIX_HOME" >> .travis.env
+
+echo "export PATH=$JAVA_HOME/bin:$WIX:$PATH" >> .travis.env
+
+echo 'export GRADLE_OPTS="-Dorg.gradle.daemon=false -Dfile.encoding=UTF-8"' >> .travis.env
+
+wget -q $JAVA_JDK_BASE_URL/$JAVA_JDK_ARCHIVE_NAME
+7z x -bd $JAVA_JDK_ARCHIVE_NAME
+
+wget -q $JPACKAGE_JDK_BASE_URL/$JPACKAGE_JDK_ARCHIVE_NAME
+7z x -bd $JPACKAGE_JDK_ARCHIVE_NAME
 
 wget -q $WIX_BASE_URL/$WIX_ARCHIVE_NAME
 7z x -bd -o$WIX_HOME wix311-binaries.zip
-echo 'export WIX=$(pwd)/$WIX_HOME' >> .travis.env
-
-echo 'export PATH="$JAVA_HOME/bin":"$WIX":$PATH' >> .travis.env
-
-echo 'export GRADLE_OPTS="-Dorg.gradle.daemon=false"' >> .travis.env
