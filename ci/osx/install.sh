fd279da4a7d59ff352eed3d83b06f1b64604ac1d
Alexander Kapitman <akman.ru@gmail.com>
2022-07-06 17:02:28 +0200

    Fixed CI settings

--- ci/osx/install.sh
+++ ci/osx/install.sh
@@ -7,6 +7,7 @@ set -ev
 JPACKAGE_JDK_BASE_URL=https://download.java.net/java/GA/jdk14.0.2/205943a0976c4ed48cb16f1043c5c647/12/GPL
 JPACKAGE_JDK_ARCHIVE_NAME=openjdk-14.0.2_osx-x64_bin.tar.gz
 JPACKAGE_JDK_HOME=jdk-14.0.2.jdk/Contents/Home
+
 wget -q $JPACKAGE_JDK_BASE_URL/$JPACKAGE_JDK_ARCHIVE_NAME
 tar xzfp $JPACKAGE_JDK_ARCHIVE_NAME
 echo "export JPACKAGE_HOME=$(pwd)/$JPACKAGE_JDK_HOME" >> .travis.env
